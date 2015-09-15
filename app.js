f0135fda786eb3c96488327fb2908d5fa8f5e363
alistaircolling <alistair.colling@gmail.com>
2015-09-15 12:00:26 +0100

    have partials working injecting views and js into the page anglar

--- app.js
+++ app.js
@@ -5,19 +5,34 @@ var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
-// add states to the state provider
-    $stateProvider
+  $urlRouterProvider.otherwise('/home');
+  // add states to the state provider
+  $stateProvider
 
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'partial-home.html'
-        })
+  // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+    url: '/home',
+    templateUrl: 'partial-home.html'
+  })
 
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            // we'll get to this in a bit
-        });
+  //nested list with custom controller - I think the scope is passed and the dogs array is attached. presumably I could specify a controller with its own js file for example that requested the data from an API
+  .state('home.list', {
+      url: '/list',
+      templateUrl: 'partial-home-list.html',
+      controller: function($scope) {
+        $scope.dogs = ['Fenella', 'Monty', 'Coco'];
+      }
+    })
+    //nested paragraph with a string passed in template. again, presumably the template could specify
+    .state('home.paragraph', {
+      url: '/paragraph',
+      template: 'This was text passed from the router config.'
+    })
+
+
+  // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+  .state('about', {
+    // we'll get to this in a bit
+  });
 
 });
